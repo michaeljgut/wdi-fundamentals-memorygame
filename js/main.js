abdc1840e9af4a16134e6205b9a473faf4c066cd
Michael Gutleber <michaeljgut@gmail.com>
2017-07-07 11:12:03 -0400

    Added shuffle cards logic

--- js/main.js
+++ js/main.js
@@ -37,6 +37,12 @@ var cardsInPlay = [];
 // score represents the number of matches found
 var score = 0;
 
+var firstCardSelected = null;	// Will hold the first card selected. Used in nextTurn to flip 
+								// the card back over.
+
+var secondCardSelected = null;	// Will hold the second card selected. Used in nextTurn to flip 
+								// the card back over.
+
 // updateScore takes the new score as a parameter and updates the global score variable, then writes
 // the new score to the screen using the textContent property.
 
@@ -57,18 +63,41 @@ var flipCard = function() {
 	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			updateScore(++score);
+			updateScore(score + 1);
 		}
 		cardsInPlay = [];
+		secondCardSelected = this;
 	}
+	else
+		firstCardSelected = this;
 }
 
-// createBoard will create the number of cards in the cards array as image elements and append them
-// to the div element with the id of "game-board." All cards will initially have their backs
-// showing. The data-id attribute holds the index in the cards array and is used in the flipCard
-// function to get the cards face value.
+// shuffleCards will use an algorithm from the https://jsperf.com/array-shuffle-comparator/5 web 
+// site which I modified to "shuffle" the cards. Pick a card at random from 0 to the length of the 
+// cards array - 1. Swap this card with the last card. Then repeat this process only picking a
+// random card from 0 to the length - 2, then 0 to the length - 3 etc. until n = 0 and all the
+// cards have been shuffled.
+
+var shuffleCards = function() {
+	var n = cards.length;
+	var i = 0;
+	var temp;
+	while (--n) { // Decrease n before checking its truthiness to skip the last case where n === 0.
+		i = Math.floor(Math.random() * n);
+		temp = cards[i];
+		cards[i] = cards[n];
+		cards[n] = temp;
+	}
+
+}
+
+// createBoard will first shuffle the cards array, then create the number of cards in the cards 
+// array as image elements and append them to the div element with the id of "game-board." All 
+// cards will initially have their backs showing. The data-id attribute holds the index in the 
+// cards array and is used in the flipCard function to get the cards face image.
 
 var createBoard = function() {
+	shuffleCards(); // Mix up the cards array
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src','images/back.png');
@@ -78,22 +107,33 @@ var createBoard = function() {
 	}
 }
 
-// resetGame will go through all the card images and set them to the back of the cards.
+// nextTurn will change the 2 selected card images to the back of the cards.
 
-var resetGame = function() {
-	var div = document.getElementById('game-board');
-	var child = div.firstChild;
-	while (child !== div.lastChild) {
-		child.setAttribute('src','images/back.png');
-		child = child.nextSibling;
-	}
-	// Set the attribute for the last card
-	child.setAttribute('src','images/back.png');
-	// Clear out cardsInPlay so that 2 cards may be selected
+var nextTurn = function() {
+	if (firstCardSelected) // Make sure the first card was selected.
+		firstCardSelected.setAttribute('src','images/back.png');
+	if (secondCardSelected) // Make sure the second card was selected.
+		secondCardSelected.setAttribute('src','images/back.png');
+	// Clear out cardsInPlay so that 2 cards may be selected again
 	cardsInPlay = [];
 }
 
+// newGame will remove all the cards from the game-board, then re-create the game-board.
+
+var newGame = function() {
+	// Remove all the cards from the game-board div
+	var divGameBoard = document.getElementById("game-board");
+	while (divGameBoard.hasChildNodes())
+		divGameBoard.removeChild(divGameBoard.firstChild);
+	createBoard();	// Recreate the game board with the shuffled cards
+	// Clear out cardsInPlay so that 2 cards may be selected again
+	cardsInPlay = [];
+	updateScore(0);
+}
+
 createBoard();
-var button = document.querySelector('button');
-// Set the reset button to execute resetGame when clicked.
-button.addEventListener('click',resetGame);
+// Set the Next Turn button to execute nextTurn when clicked.
+document.getElementById("next turn").addEventListener('click',nextTurn);
+
+// Set the New Game button to execute newGame when clicked.
+document.getElementById("new game").addEventListener('click',newGame);
